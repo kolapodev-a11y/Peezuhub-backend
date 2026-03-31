e8c8c38448fb0d4a0782efcdcf4e91481d0474bd
kolapodev-a11y <kolapodev@gmail.com>
2026-03-31 20:20:11 +0100

    Add files via upload

--- src/models/Notification.js
+++ src/models/Notification.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const notificationSchema = new mongoose.Schema(
   {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
     type: { type: String, required: true },
     title: { type: String, required: true },
     message: { type: String, required: true },
