2a310cb1c0fad6a1655a5b22225a62a92bb0d46c
kolapodev-a11y <kolapodev@gmail.com>
2026-04-03 18:12:27 +0100

    Update Nigerian states and categories constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,51 +1,71 @@
 const NIGERIAN_STATES = [
   'All States',
-  'Ondo',
-  'Oyo',
-  'Lagos',
-  'Ekiti',
-  'Osun',
-  'Ogun',
-  'ilorin',
-  'Kogi',
-  'Cross river',
-  'Abuja FCT',
-  'Rivers',
-  'Kano',
-  'Enugu',
-  'Delta',
-  'Kaduna',
-  'Anambra',
-  'Edo',
+  'Abia',
+  'Adamawa',
   'Akwa Ibom',
+  'Anambra',
+  'Bauchi',
+  'Bayelsa',
+  'Benue',
+  'Borno',
+  'Cross River',
+  'Delta',
+  'Ebonyi',
+  'Edo',
+  'Ekiti',
+  'Enugu',
+  'FCT - Abuja',
+  'Gombe',
+  'Imo',
+  'Jigawa',
+  'Kaduna',
+  'Kano',
+  'Katsina',
+  'Kebbi',
+  'Kogi',
+  'Kwara', // Ilorin is the capital here
+  'Lagos',
+  'Nasarawa',
+  'Niger',
+  'Ogun',
+  'Ondo',
+  'Osun',
+  'Oyo',
   'Plateau',
-  'Benin',
-  'Porthacourt',
-  'Niger'
-  
-
+  'Rivers', // Port Harcourt is the capital here
+  'Sokoto',
+  'Taraba',
+  'Yobe',
+  'Zamfara'
 ];
 
 const CATEGORIES = [
-  'Graphic Design',
-  'Food Delivery',
+  'Brand-new Phones & Accessories',
+  'Second-hand Phones',
+  'Computers & Accessories',
+  'Solar & Generators',
+  'Groceries & Provisions',
+  'Fashion & Clothing',
   'Car Sales',
+  'Car Rentals',
   'House Sales',
   'Land Sales',
-  'Second-hand Phones',
-  'Fashion & Clothing',
-  'Photography & Video',
-  'Beauty & Bodycare',
-  'Poultry Sales',
   'Furniture & Home Furniture Sales',
-  'Gaming Console'
+  'Event Planning & Rentals',
+  'Poultry Sales',
+  'Beauty & Bodycare',
+  'Gaming Console',
+  'Kitchen & Home Appliances',
+  'Photography & Video'
 ];
 
 const SCAM_KEYWORDS = [
   'send money first',
   'gift card',
   'western union',
-  'advance payment'
+  'advance payment',
+  'pay before delivery',
+  'investment doubled'
 ];
 
 const SAFETY_DISCLAIMER = 'Payments happen outside the platform (WhatsApp, bank transfer, OPay, PalmPay). PeezuHub does not hold funds — deal at your own risk and verify sellers.';
